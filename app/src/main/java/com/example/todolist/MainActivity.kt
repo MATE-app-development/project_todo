ed3f4846ff281015200eb63e9ae2b3305c3bcb17
YEERRin <ryeon0107@gmail.com>
2023-01-23 16:59:17 +0900

    2023.01.18 ver project main

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -95,8 +95,8 @@ class MainActivity : AppCompatActivity() {
             DatePickerDialog(this, dateSetListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH),
                 cal.get(Calendar.DAY_OF_MONTH)).show()
         })
-
-
+        
+        //깃허브개짱
 
 
 
