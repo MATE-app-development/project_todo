ca9672f21ade2b625576952f1ebf42c5743048b9
YEERRin <ryeon0107@gmail.com>
2023-02-01 18:58:34 +0900

    2023.02.01 update version

--- app/src/main/java/com/example/mate/MainActivity.kt
+++ app/src/main/java/com/example/mate/MainActivity.kt
@@ -1,66 +1,98 @@
 package com.example.mate
 
+
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.support.annotation.RequiresApi
+import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
+import android.widget.CheckBox
+import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import java.time.LocalDate
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var timeanddate: TextView
+    lateinit var check1: CheckBox
+    lateinit var check2: CheckBox
+    lateinit var check3: CheckBox
+    lateinit var addButton: FloatingActionButton
+    var todayDate=""
+
+    @RequiresApi(Build.VERSION_CODES.N)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         dbHelper = SqliteHelper(this, "todo.db", null, 1)
 
-    }
+        timeanddate = findViewById(R.id.timeanddate)
+        check1 = findViewById(R.id.check1)
+        check2 = findViewById(R.id.check2)
+        check3 = findViewById(R.id.check3)
+        addButton = findViewById(R.id.addButton)
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    override fun onStart() {
-        super.onStart()
-        var selecteddate = LocalDate.now()
-        editTextDate.text = selecteddate.toString()
 
-        var resulttodolist = dbHelper.selectalltodo(dbHelper.readableDatabase, null)
-        textView1.text = resulttodolist[0].tID.toString()
+        //상단 오늘 날짜 가져오기
+        val now_time : Long = System.currentTimeMillis()
+        val date_time = Date(now_time) //Date 타입으로 변경
+        val todaydate = SimpleDateFormat("yyyy-MM-dd") //원하는 포맷으로 지정
+        val str_todaydate = todaydate.format(date_time)
+        timeanddate.text = str_todaydate.toString()
 
-        clickButton.setOnClickListener {
-            val intent = Intent(this, Popup_Todo_Add::class.java)
-            startActivity(intent)
+
+        //checkbox 체크 여부 확인
+
+        check1.setOnCheckedChangeListener { buttonView, isChecked ->
+
+            if(isChecked){
+                //정렬 알고리즘 사용, 맨 아래로 정렬
+                Toast.makeText(this,"complete task1", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                //정렬 알고리즘 사용
+                Toast.makeText(this,"Do not complete task1", Toast.LENGTH_SHORT).show()
+            }
         }
 
-        textView1.setOnClickListener {
-            val intent = Intent(this, Popup_Todo_Update::class.java)
-            intent.putExtra("tID", textView1.text.toString())
-            startActivity(intent)
-        }
-    }
+        check2.setOnCheckedChangeListener { buttonView, isChecked ->
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    override fun onResume() {
-        super.onResume()
-        var selecteddate = LocalDate.now()
-        editTextDate.text = selecteddate.toString()
-
-        var resulttodolist = dbHelper.selectalltodo(dbHelper.readableDatabase, null)
-        textView1.text = resulttodolist[0].tID.toString()
-
-        clickButton.setOnClickListener {
-            val intent = Intent(this, Popup_Todo_Add::class.java)
-            startActivity(intent)
+            if(isChecked){
+                //정렬 알고리즘 사용, 맨 아래로 정렬
+                Toast.makeText(this,"complete task1", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                //정렬 알고리즘 사용
+                Toast.makeText(this,"Do not complete task1", Toast.LENGTH_SHORT).show()
+            }
         }
 
-        textView1.setOnClickListener {
-            val intent = Intent(this, Popup_Todo_Update::class.java)
-            intent.putExtra("tID", textView1.text.toString())
-            startActivity(intent)
+        check3.setOnCheckedChangeListener { buttonView, isChecked ->
+
+            if(isChecked){
+                //정렬 알고리즘 사용, 맨 아래로 정렬
+                Toast.makeText(this,"complete task1", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                //정렬 알고리즘 사용
+                Toast.makeText(this,"Do not complete task1", Toast.LENGTH_SHORT).show()
+            }
         }
+
+        //추가 버튼 클릭시 화면 전환
+        addButton.setOnClickListener {
+            startActivity(Intent(this@MainActivity, Popup_Todo_Add::class.java))
+        }
+
+        
     }
 
-    override fun onDestroy() {
-        dbHelper.close()
-        super.onDestroy()
-    }
+
+
+
 }
